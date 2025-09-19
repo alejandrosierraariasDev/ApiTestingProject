1200710397bc6b9b1a1a489e1ca38f90439c8c8b
Alejandro <alejandrosierraarias@gmail.com>
2025-09-19 14:53:20 +0200

    Updated pom to use the URL as a env variable in tests

--- src/test/java/com/example/tests/SimpleApiTest.java
+++ src/test/java/com/example/tests/SimpleApiTest.java
@@ -1,26 +1,25 @@
-package com.example.tests;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-
-
 public class SimpleApiTest {
-    @Test
-    public void testGetSingleUser() {
-        // Define la URL base de la API
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
 
-        // Realiza una petición GET al endpoint /todos/1
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = System.getProperty("base.uri", "https://jsonplaceholder.typicode.com");
+    }
+
+    @Test
+    public void testGetTodo() {
         given()
-            .header("Content-Type", "application/json")
-            .header("Accept", "application/json")
-        .when()
-            .get("/todos/1")
-        .then()
-            .statusCode(200) // Valida que el código de estado sea 200 (OK)
-            .body("id", equalTo(1)) // Valida que el ID del todo sea 1
-            .body("title", equalTo("delectus aut autem")); // Valida el título del todo
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/todos/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("title", equalTo("delectus aut autem"));
     }
 }
